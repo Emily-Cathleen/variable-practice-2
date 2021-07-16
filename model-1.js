12dc9ebff7c65dd9e8d73f80891fc2f1205a0673
Emily Corcoran <cathleencorcoran@gmail.com>
2021-07-15 23:55:14 -0400

    Add array

--- model-1.js
+++ model-1.js
@@ -10,3 +10,17 @@ Number Vairables
 var henryAge = 2;
 var emilyAge = 30;
 var tomAge = 32;
+
+Boolean Variables
+
+var isDog = true;
+var isCat = false;
+var isHuman = false;
+
+Array Variables
+
+var favoriteThings = ['Henry', 'Veggie Burgers', 'Robb', 'Lucinda'];
+var goodPlan = ['exercise', 'routine', 'morning ritual'];
+var trees = ['pine', 'sprouce', 'ginko'];
+
+Object Variables
